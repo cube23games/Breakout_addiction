3d327c9374eeb6097cefdc0abe92212fd3b2406e
lindsey-wells <lindsey-wells@users.noreply.github.com>
2026-07-16 20:55:33 -0400

    BA-56G improve notification logo recognition

--- tools/patch_android_notifications.py
+++ tools/patch_android_notifications.py
@@ -50,13 +50,14 @@ RECEIVERS = """        <receiver
 # Android notification small icons are monochrome system-tinted masks.
 # This drawable restores the actual Breakout Addiction brand mark as a
 # simplified silhouette:
-# - broken circular barrier
-# - bold center path breaking through
-# - a few large breakout shards at the top
+# - visibly broken circular barrier with asymmetric arc endings
+# - wider, more decisive center path breaking through
+# - two larger detached breakout shards
+# - clearer negative space through the top opening
 # - white artwork only
 # - no dark square background
-# - no gradients, shadows, or tiny detail
-# - refined thinner brand silhouette proportions
+# - no gradients, shadows, strokes, or tiny detail
+# - recognition-focused brand silhouette geometry
 VECTOR_ICON_XML = """<?xml version="1.0" encoding="utf-8"?>
 <vector xmlns:android="http://schemas.android.com/apk/res/android"
     android:width="24dp"
@@ -66,65 +67,58 @@ VECTOR_ICON_XML = """<?xml version="1.0" encoding="utf-8"?>
 
     <path
         android:fillColor="#FFFFFFFF"
-        android:pathData="M6.2,18.0
-            C4.7,16.5 3.8,14.3 3.8,11.8
-            C3.8,7.8 6.4,4.4 10.2,3.4
-            L10.6,4.9
-            C7.6,5.8 5.6,8.5 5.6,11.8
-            C5.6,13.3 6.0,14.6 6.8,15.7
-            L7.8,14.9
-            L8.3,17.0
-            C7.6,17.6 6.9,18.0 6.2,18.0
+        android:pathData="M6.3,18.1
+            C4.8,16.7 3.9,14.5 3.9,11.9
+            C3.9,8.0 6.2,4.9 9.5,3.9
+            L10.0,5.4
+            C7.6,6.2 5.7,8.7 5.7,11.9
+            C5.7,13.5 6.2,14.9 7.0,15.9
+            L8.0,15.0
+            L8.4,17.2
+            C7.7,17.7 7.0,18.1 6.3,18.1
             Z" />
 
     <path
         android:fillColor="#FFFFFFFF"
-        android:pathData="M17.8,18.0
-            C19.3,16.5 20.2,14.3 20.2,11.8
-            C20.2,7.8 17.6,4.4 13.8,3.4
-            L13.4,4.9
-            C16.4,5.8 18.4,8.5 18.4,11.8
-            C18.4,13.3 18.0,14.6 17.2,15.7
-            L16.2,14.9
-            L15.7,17.0
-            C16.4,17.6 17.1,18.0 17.8,18.0
+        android:pathData="M17.5,18.4
+            C19.1,16.9 20.0,14.6 20.0,11.8
+            C20.0,8.0 17.9,5.1 14.6,3.9
+            L14.0,5.4
+            C16.4,6.3 18.1,8.7 18.1,11.8
+            C18.1,13.5 17.7,14.9 16.8,16.1
+            L15.8,15.1
+            L15.4,17.4
+            C16.1,18.0 16.9,18.4 17.5,18.4
             Z" />
 
     <path
         android:fillColor="#FFFFFFFF"
-        android:pathData="M11.4,20.6
-            L9.2,19.3
-            L10.6,14.4
-            C10.9,13.4 11.0,12.6 10.8,11.8
-            C10.5,10.7 9.8,9.8 9.0,9.0
-            L10.4,6.9
-            C12.0,8.1 13.4,9.6 13.8,11.5
-            C14.2,13.4 13.4,15.2 12.8,16.8
-            L14.2,16.8
-            L11.2,21.0
+        android:pathData="M11.4,20.8
+            L9.0,19.2
+            L10.4,14.2
+            C10.8,13.1 10.9,12.3 10.6,11.3
+            C10.3,10.1 9.4,9.2 8.7,8.5
+            L10.2,6.3
+            C12.2,7.7 13.8,9.4 14.3,11.5
+            C14.8,13.6 13.9,15.5 13.2,17.1
+            L14.8,17.0
+            L11.4,20.8
             Z" />
 
     <path
         android:fillColor="#FFFFFFFF"
-        android:pathData="M9.4,2.4
-            L8.3,3.7
-            L9.6,4.2
-            L10.1,3.1
+        android:pathData="M9.0,1.6
+            L7.8,2.9
+            L9.5,3.5
+            L10.2,2.2
             Z" />
 
     <path
         android:fillColor="#FFFFFFFF"
-        android:pathData="M12.0,1.9
-            L11.6,4.2
-            L12.4,4.2
-            Z" />
-
-    <path
-        android:fillColor="#FFFFFFFF"
-        android:pathData="M14.6,2.4
-            L13.9,3.1
-            L14.4,4.2
-            L15.7,3.7
+        android:pathData="M14.8,1.5
+            L13.6,2.6
+            L14.7,3.6
+            L16.1,2.6
             Z" />
 
 </vector>
